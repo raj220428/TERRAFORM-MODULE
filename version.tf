1428303b4c5586dca6f3b243912571f60c805453
raj220428 <64214330+raj220428@users.noreply.github.com>
2022-03-26 00:43:57 +0530

    Add files via upload

--- version.tf
+++ version.tf
@@ -6,28 +6,15 @@ terraform {
       version = "~> 4.0"
     }
   }
-
-  backend "s3" {
-    bucket = "terraform280422"
-    key    = "prod/ryan.tfstate"
-    region = "ap-south-1"
-
-    dynamodb_table = "terraform-prod-state-table"
-  }
 }
 
-/*provider "aws" {
-  #profile     = "newterraform"
-  region = "ap-south-1"
-  #region = var.region*/
+provider "aws" {
+  #profile = "newterraform"
+  region  = "ap-south-1"
+  #region = var.region
+}
 
 
-/*terraform {
-  backend "local" {
-    path = "./raj/terraform/terraform.tfstate"
-  }
-
-}*/
 
 
 
